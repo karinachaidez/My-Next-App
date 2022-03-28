ea6fe7ca85a8eaefab9f7086afb8caa091f8c74b
Karina Chaidez <101217689+karinachaidez@users.noreply.github.com>
2022-03-28 14:50:01 -0700

    edit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello to <a href="https://nextjs.org">Next.js!</a>
+          Hi to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
